233548213c62790ea988253723d78c227628652b
Yemi-21 <yemsrachbeza@gmail.com>
2022-10-19 14:32:46 -0700

    Python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string(l=[]):
-
-        l += ["BestSchool"]
-
-            return ", ".join(l)
+def magic_string():
+        magic_string.ntimes = getattr(magic_string, 'ntimes', 0) + 1
+            return ("BestSchool, " * (magic_string.ntimes - 1) + "BestSchool")
