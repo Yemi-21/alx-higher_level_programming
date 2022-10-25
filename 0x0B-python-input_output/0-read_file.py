ad89a840e8d961ad6c443929f6097945fa05b06d
Yemi-21 <yemsrachbeza@gmail.com>
2022-10-25 17:29:09 +0000

    READ

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
-"""read_file module """
+
+""" read_file module """
+
+
+
 def read_file(filename=""):
+
     """
+
         function that read specific file
+
         Args:
+
             filename: name of the file
+
     """
-  with open(filename, encoding="UTF-8") as file:
+
+    with open(filename, encoding="UTF-8") as file:
+
         read_data = file.read()
+
         print(read_data, end="")
+
     file.closed
