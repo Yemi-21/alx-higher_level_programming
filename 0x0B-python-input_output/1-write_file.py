4eacce2f1cdd7addbf0f8d71be7721c45a8fa884
Yemi-21 <yemsrachbeza@gmail.com>
2022-10-25 13:35:45 +0000

    Write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,11 +4,11 @@ def append_write(filename="", text=""):
 """
         function that write a specific text in a specific file
         Args:
-            fileName: name of the file
+            filename: name of the file
             text: text to write
     """   
     nbCharacter = 0
-    with open(fileName, 'w', encoding="UTF-8") as file:
+    with open(filename, 'w', encoding="UTF-8") as file:
         nbCharacter = file.write(text)
     file.closed
     return nbCharacter
