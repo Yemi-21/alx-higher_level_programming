d194548f1fed37f91ab181ed924afbff22c6792c
Yemi-21 <yemsrachbeza@gmail.com>
2022-10-25 14:04:34 +0000

    Write

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ read_file module """
+
+
 def read_file(fileName=""):
  """
         function that read specific file
+
         Args:
             fileName: name of the file
     """
