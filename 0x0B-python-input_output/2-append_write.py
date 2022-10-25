ac51ae87f19785b5f462839ac4b9dcabb874aed7
Yemi-21 <yemsrachbeza@gmail.com>
2022-10-25 13:54:02 +0000

    Append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+""" append_write module """
+
+
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+    """
+        function that add specific text to a specific file
+
+        Args:
+            filename: name of the file
+            text: text to add
+    """
+    nbCharacter = 0
+    with open(filename, 'a', encoding="UTF-8") as file:
+        nbCharacter = file.write(text)
+    file.closed
+    return nbCharacter
