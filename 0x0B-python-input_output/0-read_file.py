6a54286125b75243575cc3d2dff28876abbb1d4c
Yemi-21 <yemsrachbeza@gmail.com>
2022-10-25 12:47:51 +0000

    Write

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ read_file module """
 def read_file(fileName=""):
-"""
+    """
         function that read specific file
         Args:
-            prmFileName: name of the file
+            fileName: name of the file
     """
-   with open(fileName, encoding="UTF-8") as file:
+    with open(fileName, encoding="UTF-8") as file:
         read_data = file.read()
         print(read_data, end="")
     file.closed
