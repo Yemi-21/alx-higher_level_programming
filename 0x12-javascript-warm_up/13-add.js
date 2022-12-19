579b1931d230905667c1c72910fac8fee44bddd6
Yemi-21 <Yemsrachbeza@gmail.com>
2022-12-19 12:34:41 +0000

    Java

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -2,10 +2,12 @@
 
 /**
  * Add two numbers
+ *
  * @param {*} a first number
  * @param {*} b second number
+ *
  * @returns result of addition of two parameters
  */
  exports.add = function (a, b) {
   return (a + b);
-}
+};
