3e1e02cd0ab20c4c7b07318ffeb07baaf6549bb5
Yemi-21 <Yemsrachbeza@gmail.com>
2022-12-20 13:49:40 +0000

    Python

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""    
-    script that lists all State objects
-    that contain the letter a from the database hbtn_0e_6_usa
+"""
+script that lists all State objects
+that contain the letter a from the database hbtn_0e_6_usa
 """
 
 
