7f78e635c2635357e5ff88557f6b00d56f71fadb
Yemi-21 <Yemsrachbeza@gmail.com>
2022-12-21 08:46:19 +0000

    Javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-const BaseSquare = require('./5-square');
-
-module.exports = class Square extends BaseSquare {
-  charPrint (prmCharacter) {
-    if (prmCharacter === undefined) {
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
       this.print();
     } else {
-      for (let row = 0; row < this.height; row++) {
-        for (let column = 0; column < this.width; column++) {
-          process.stdout.write(prmCharacter);
-        }
-        process.stdout.write('\n');
-      }
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
