1f5d08eb862c9ba1ff7396ec17a6b924b8fbd409
Yemi-21 <Yemsrachbeza@gmail.com>
2022-12-21 08:35:53 +0000

    Javascript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,22 +1,19 @@
 #!/usr/bin/node
 
-/**
- * Rectangle class
- */
 module.exports = class Rectangle {
-  constructor (prmWidth, prmHeight) {
-    if (prmWidth > 0 && prmHeight > 0) {
-      this.width = prmWidth;
-      this.height = prmHeight;
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
-    for (let row = 0; row < this.height; row++) {
-      for (let column = 0; column < this.width; column++) {
-        process.stdout.write('X');
-      }
-      process.stdout.write('\n');
-    }
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
